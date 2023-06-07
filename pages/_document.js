ea3b7e1c3b715fa99d5c61fd69cf8abd2d521c18
Gautam soni <62197924+gautamsoni8132@users.noreply.github.com>
2023-06-07 22:57:57 +0530

    3rd Update fornt

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useState } from 'react'
 export default function Document() {
-
-  const [div,setDiv]=useState(0);
-
-  
-  const stopLoader = () => {
-    setDiv(1);
-  }
   return (
-    <Html lang="en" onLoad={stopLoader}>
-
+    <Html lang="en" >
+ <Head />
       <body>
-        <div style={{"display":div==0?"none":"block","zIndex":"9999999999999999999999999999", "width": "100%", "height": "100vh", "backgroundColor": "#fff","textAlign":"center","padding":"25% 0%","position":"fixed" }}>
-          <img src="/loader.gif" alt="loader" style={{"margin":"0% auto"}} />
-        </div>
-        <Head />
         <Main />
         <NextScript />
       </body>
